d160300013de00e2828f403ff43837a9e8336d2e
andycapn <aundreycap@gmail.com>
2020-04-19 02:25:41 +0200

    - Began work on the next section of the page

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,14 +28,6 @@ module.exports = {
         icon: `src/images/nkoba-logo.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: /src/,
-        },
-      },
-    },
 
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import './Styles/index.css'
+import "./Styles/index.css"
 import Cta from "../components/call-to-action/cta"
 import nkobaHead from "../images/nkoba-head.svg"
+import nkobaWarrior from "../images/SVG/Asset 4.svg"
+import { css } from "@emotion/core"
 
 const IndexPage = () => (
   <Layout>
@@ -12,22 +14,38 @@ const IndexPage = () => (
       <section className={"above-fold"}>
         <section className={"hero-area"}>
           <h1 className={"hero-h1"}>
-            Digital Marketing For <br/> Forward Thinking SMEs <br/> & Entrepreneurs.
+            Digital Marketing For <br/> Forward Thinking SMEs <br/> &
+            Entrepreneurs.
           </h1>
           <p className={"hero-text"}>
-            We want to help you build your empire. We do digital <br/> marketing that makes an impact.
+            We want to help you build your empire. We do digital <br/>{" "}
+            marketing that makes an impact.
           </p>
           <Cta/>
         </section>
-        <img src={nkobaHead} className='hero-image'/>
+        <img src={nkobaHead} className="hero-image" alt=""/>
       </section>
-      <section className='container'>
-        <aside>
-          <h1>Conquer your competition with marketing that works</h1>
+      <section className="container">
+        <aside className="offer-area">
+          <h2
+            css={css`
+              font-size: 32px;
+              font-family: "Montserrat", sans-serif;
+              font-weight: bold;
+            `}
+          >
+            Conquer your competition with marketing that works
+          </h2>
         </aside>
+        <img
+          src={nkobaWarrior}
+          css={css`
+            width: 476px;
+            height: 609px;
+          `}
+          alt=""
+        />
       </section>
-
-
     </>
   </Layout>
 )
